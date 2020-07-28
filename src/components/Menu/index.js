5256c87286c90433465473b23278e1b506f7b66c
Filipe <filipe.souza1602@gmail.com>
2020-07-28 03:06:18 -0300

    First Commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../../assets/img/souzaFlix.png'
+import './Menu.css'
+import ButtonLink from './components/ButtonLink'
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="Souzaflix logo" />
+            </a>
+
+            <ButtonLink className="ButtonLink" href="/">
+                Novo Video
+            </ButtonLink>
+        </nav>
+    );
+}
+
+export default Menu;
